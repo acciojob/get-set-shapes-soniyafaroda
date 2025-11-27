5185896d20e4c3e4567288bf2b9cf98b0f3d1a09
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-27 17:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-//complete this code
-class Rectangle {}
+// -----------------------------
+// Rectangle Class
+// -----------------------------
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// -----------------------------
+// Square Class (inherits Rectangle)
+// -----------------------------
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // width = side, height = side
+    this._side = side;
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this._side;
+  }
+}
+
+// -----------------------------
+// Do not change the code below
+// -----------------------------
 window.Rectangle = Rectangle;
 window.Square = Square;
